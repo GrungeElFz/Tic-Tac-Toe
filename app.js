836ae083789e389333d4dcfc606db958270a3128
GrungeElFz <elf0114_@hotmail.com>
2022-08-21 20:49:07 -0500

    Added: Functions in app.js

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 // STATE
 const gameState = {
     players: ['x', 'o'],
-    board: [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null]
-    ]
   };
-let turn = gameState.players[0];
 
-
-
-
+  
+  
 // DOM SELECTORS
 const board = document.getElementById('board');
 const cells = document.querySelectorAll('[data-index]');
 const resetButton = document.getElementById('reset-button');
+const afterMatchMessage = document.getElementById('after-match-message');
+  
+let turn = gameState.players[0];
 
+const boardState = Array(cells.length);
+boardState.fill(null);
 
 
 
@@ -24,10 +22,43 @@ const resetButton = document.getElementById('reset-button');
 
 
 
-
 // HELPER FUNCTIONS
 const clickChecker = (event) => {
     console.log('Clicked');
+}
+
+const cellHover = () => {
+    cells.forEach(cell => {
+        cell.classList.remove('x-hover');
+        cell.classList.remove('o-hover');
+    });
+
+    const hoverClass = `${turn}-hover`;
+
+    cells.forEach((cell) => {
+        if (cell.innerText === '') {
+            cell.classList.add(hoverClass);
+        }
+    });
+}
+
+const cellClick = (event) => {
+    const cell = event.target;
+    const cellIndex = cell.dataset.index;
+
+    if (!cell.innerText === '') {
+        return;
+    }
+
+    if (turn === gameState.players[0]) {
+        cell.innerText = gameState.players[0];
+        boardState = gameState.players[0];
+        turn = gameState.players[1];
+    } else {
+        cell.innerText = gameState.players[1];
+        boardState = gameState.players[1];
+        turn = gameState.players[0];
+    }
 }
 
 
@@ -42,3 +73,4 @@ cells.forEach(cell => {
 
 
 // BOOTSTRAP
+cellHover()
